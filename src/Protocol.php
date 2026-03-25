4d49a0542455c74443fcbec030f17337cbe22e94
Jake Barnby <jakeb994@gmail.com>
2026-03-25 17:23:30 +1300

    (feat): Expand protocol support to 25

--- src/Protocol.php
+++ src/Protocol.php
@@ -10,4 +10,26 @@ enum Protocol: string
     case PostgreSQL = 'postgresql';
     case MySQL = 'mysql';
     case MongoDB = 'mongodb';
+    case Redis = 'redis';
+    case Memcached = 'memcached';
+    case Kafka = 'kafka';
+    case AMQP = 'amqp';
+    case ClickHouse = 'clickhouse';
+    case Cassandra = 'cassandra';
+    case NATS = 'nats';
+    case MSSQL = 'mssql';
+    case Oracle = 'oracle';
+    case Elasticsearch = 'elasticsearch';
+    case MQTT = 'mqtt';
+    case GRPC = 'grpc';
+    case ZooKeeper = 'zookeeper';
+    case Etcd = 'etcd';
+    case Neo4j = 'neo4j';
+    case Couchbase = 'couchbase';
+    case CockroachDB = 'cockroachdb';
+    case TiDB = 'tidb';
+    case Pulsar = 'pulsar';
+    case FTP = 'ftp';
+    case LDAP = 'ldap';
+    case RethinkDB = 'rethinkdb';
 }
